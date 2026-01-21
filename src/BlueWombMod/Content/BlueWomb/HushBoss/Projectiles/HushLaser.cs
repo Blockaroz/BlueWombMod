58cd9def0f19466831b54fd92fa1cc0eee48cc31
Blockaroz <lbpk.123fan@gmail.com>
2026-01-20 23:29:23 -0500

    feat: lasers ai

--- src/BlueWombMod/Content/BlueWomb/HushBoss/Projectiles/HushLaser.cs
+++ src/BlueWombMod/Content/BlueWomb/HushBoss/Projectiles/HushLaser.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 
 namespace BlueWombMod.Content.BlueWomb.HushBoss.Projectiles;
@@ -18,6 +20,45 @@ public sealed class HushLaser : ModProjectile
     public override void SetDefaults()
     {
         
+    }
+
+    public ref float HostIndex => ref Projectile.ai[0];
+
+    public Hush Hush
+    {
+        get
+        {
+            if (HostIndex >= 0 && HostIndex < Main.npc.Length)
+            {
+                NPC npc = Main.npc[(int)HostIndex];
+                if (npc.ModNPC is Hush hush)
+                    return hush;
+            }
+
+            return null;
+        }
+    }
+
+    public ref float Time => ref Projectile.ai[1];
+
+    public override void OnSpawn(IEntitySource source)
+    {
+        HostIndex = -1;
+    }
+
+    public override void AI()
+    {
+        if (Hush == null)
+        {
+            return;
+        }
+
+        var target = Hush.NPC.GetTargetData();
+
+        if (!target.Invalid)
+        {
+
+        }
     }
 
     public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
